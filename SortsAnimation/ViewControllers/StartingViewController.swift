5e3eed038d087411503903667e77cf8607a75e2c
OleksiiKolomiiets <Oleksii_Kolomiiets@epam.com>
2018-05-07 14:43:14 +0300

    made a connection between Starting and Visualization VCs by the buttons touch up

--- SortsAnimation/ViewControllers/StartingViewController.swift
+++ SortsAnimation/ViewControllers/StartingViewController.swift
@@ -15,9 +15,21 @@ class StartingViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tappedTypeOfSort(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let targetVC = storyboard.instantiateViewController(withIdentifier: "VisualizationId") as? VisualizationViewController else { return }
+        self.navigationController?.pushViewController(targetVC, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let targetVC = segue.destination as? VisualizationViewController {
+//            
+//        }
     }
 
 
