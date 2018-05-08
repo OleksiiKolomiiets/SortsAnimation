715f7da7d9242b45353870465d36e991024e1d7e
OleksiiKolomiiets <Oleksii_Kolomiiets@epam.com>
2018-05-08 18:33:43 +0300

    updated gitignore

--- SortsAnimation/Models/SelectionModel.swift
+++ SortsAnimation/Models/SelectionModel.swift
@@ -30,21 +30,27 @@ class SelectionModel: SortModelProtocol {
     
     private var highlight = [(Int, Int)]()
     
-    func fakeSort(_ array: [Int], itterationStep: Int) -> SortResult {
-        var copy = array
+    func fakeSort2(_ array: [Int], itterationStep: Int) -> SortResult {
+        var copy = array {
+            didSet {
+                
+            }
+        }
         var minIndex = 0
         let sortedArray = sort(copy)
         
         for index1 in 0..<copy.count {
             minIndex = index1
-            for index2 in itterationStep..<copy.count {
+            for index2 in index1..<copy.count {
                 highlight.append((minIndex, index2))
-                if copy[minIndex] < copy[index2] {
+                if copy[minIndex] > copy[index2 + 1] {
                     minIndex = index2
+                    return .swap(first: index1, second: minIndex, interiumResult: copy)
                 }
-                return .highlight(first: minIndex, second: index2, itterationStep: index2 + 1)
+                return .highlight(first: index2, second: minIndex, itterationStep: index2 + 1)
             }
             copy.swapAt(index1, minIndex)
+            
             if sortedArray.isEqual(copy) {
                 return .swap(first: index1, second: minIndex, interiumResult: copy)
             } else {
@@ -52,5 +58,25 @@ class SelectionModel: SortModelProtocol {
             }
         }
         return .end(highlight: [(1, 2)])
+    }
+    
+    func fakeSort(_ array: [Int], itterationStep: Int) -> SortResult {
+        var copy = array
+        var minIndex = 0
+        
+        for index1 in 0..<copy.count {
+            minIndex = index1
+            let index3 = index1 + 1
+            for index2 in index3..<copy.count {
+                if copy[minIndex] > copy[index2] {
+                    minIndex = index2
+                }
+            }
+            highlight.append((minIndex, index1))
+            copy.swapAt(index1, minIndex)
+            print(minIndex)
+            return .swap(first: index1, second: minIndex, interiumResult: copy)
+        }
+        return .end(highlight: highlight)
     }
 }
